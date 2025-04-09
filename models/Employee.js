dbcbe43f3d11cd2a6716f8cd57abf856577b2cb7
Ardra-WAC <ardra.m@webandcrafts.in>
2025-04-09 14:44:14 +0530

    Update Employee.js

--- models/Employee.js
+++ models/Employee.js
@@ -1,21 +1,3 @@
-// import mongoose from "mongoose";
-
-// const EmployeeSchema = new mongoose.Schema({
-//     email: {
-//         type: String,
-//         required: [true, "email is a required"],
-//         unique: true,
-//         trim: true
-//     },
-//     role: {
-//         type: String,
-//         required: [true, "Role is required"],
-//         trim: true
-//     }
-// });
-
-// export default mongoose.model("Employee", EmployeeSchema);
-
 const mongoose = require('mongoose');
 
 const employeeSchema = new mongoose.Schema({
